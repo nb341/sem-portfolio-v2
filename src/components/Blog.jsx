41b4445f66a1744a85a551c15ba6fe5cfae85b65
Narindra Balkissoon <narindra_balkissoon@outlook.com>
2025-05-05 13:30:07 -0500

    updated package.json

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Text, Link, Image, ButtonGroup, Button } from "@chakra-ui/react";
 
 const BlogItem = (props) => {
-    const {img, description, tags, title, code, demo} = props;
+    const {description, tags, title, code, demo} = props;
     return(
         <Box 
                 display={'flex'} 
@@ -18,51 +18,14 @@ const BlogItem = (props) => {
                 boxShadow={'dark-lg'}
                 mt={'32px'}
                 >   
-               
-                <Image h={'274px'} w={'322px'} src={img} borderRadius={'lg'} mr={{ lg:'32px'}} opacity={0.8}/>
-                <Box width={'100%'} mt={'3px'}>
-                    <ButtonGroup variant={'link'} marginBottom={'25px'} lineHeight={'20px'}>
-                        {
-                            tags.map((tag, i)=>{
-                                return(
-                                    <Button key={i} fontSize={'16px'} color={'yellow.300'}>#{tag}</Button>
-                                )
-                            })
-                        }
-                    </ButtonGroup>
+           
+
                     <Text fontWeight={'500'} lineHeight={'30x'} fontSize={'24px'} marginBottom={'18px'} color={'pink.400'}>{title}</Text>
                     <Text w={{sm: 'sm', lg: 'lg'}} fontWeight={'500'} lineHeight={'20px'} fontSize={'16px'} color={'yellow.200'} marginBottom={'51px'}>
                         {description}
                     </Text>
-                    <ButtonGroup >
-                        <Link href={demo} isExternal>
-                            <Button 
-                                fontSize={'18px'} 
-                                px={'40px'}
-                                py={'13px'}
-                                lineHeight={'21px'}
-                                bgColor={'pink.500'}
-                                borderRadius={'lg'}
-                                fontWeight={'semibold'}
-                                >
-                                Demo
-                            </Button>
-                        </Link>
-                        <Link href={code} isExternal>
-                            <Button 
-                                fontSize={'18px'}
-                                px={'39px'}
-                                py={'12px'}
-                                variant={'outline'}
-                                colorScheme={'pink'}
-                                borderRadius={'lg'}
-                                fontWeight={'semibold'}
-                                >
-                                Code
-                            </Button>
-                        </Link>
-                    </ButtonGroup>
-                </Box>
+                   
+               
             </Box>
     )
 }
@@ -94,16 +57,16 @@ export default function Blog(){
                 code={"https://github.com/nb341/Image-Uploader"}
                 demo={"https://image-uploader-08jw.onrender.com/"}
                 
-                title={'Image Uploader'}
-                description={'In this project I use React.js and CSS for the frontend as well as Redux for state management. I also use NodeJS to handle upload and storage to the server.'}
+                title={`Why AI can't replace software engineers`}
+                description={'While AI has made significant progress in code generation, testing, and automation, it’s far from replacing software engineers. Writing code is only a fraction of what software engineers do—they design systems, solve complex problems, understand user needs, and make architectural decisions that require creativity, critical thinking, and context. AI tools like Copilot can assist by speeding up repetitive tasks, but they lack the deep understanding, judgment, and adaptability needed to navigate real-world software projects. Rather than replacing engineers, AI enhances their capabilities, making them more efficient and allowing them to focus on higher-level problem solving.'}
                 />
             <BlogItem 
                 tags={['JSX','ReactJS', 'NodeJS','CSS', 'Redux']}
                 code={"https://github.com/nb341/Image-Uploader"}
                 demo={"https://image-uploader-08jw.onrender.com/"}
                
-                title={'Image Uploader'}
-                description={'In this project I use React.js and CSS for the frontend as well as Redux for state management. I also use NodeJS to handle upload and storage to the server.'}
+                title={`Why .NET is better than Spring`}
+                description={'Many developers find .NET, particularly ASP.NET Core, more streamlined and easier to work with compared to Spring due to its modern, integrated tooling, excellent performance, and cross-platform support. .NET’s tight integration with Visual Studio, built-in dependency injection, and minimal configuration make it faster to get projects up and running. ASP.NET Core often outperforms Spring Boot in benchmarks, and C# as a language is considered more concise and expressive than Java by some. Additionally, Microsoft’s strong backing, extensive documentation, and growing open-source ecosystem make .NET a compelling choice for building scalable, high-performance web applications.'}
                 />
                 
         </Box>
